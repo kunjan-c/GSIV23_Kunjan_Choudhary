23aeb9573e5430fbebc16b18363be4c55c004281
Kunjan <kunjan@controlshift.in>
2023-08-24 05:14:41 +0530

    listing api integrated in UI

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ API KEY : e775d9d385ca8f76409a2109ba8ad621
 
 API Read Access Token :
 eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNzc1ZDlkMzg1Y2E4Zjc2NDA5YTIxMDliYThhZDYyMSIsInN1YiI6IjY0ZTY0Y2I2MDZmOTg0MDBjYTU0MzlmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.04UAMm8aaZXdsRCo8qmpzAzxs_2KRUDosFtybkGAsGA
+
+<!-- 
+ 'https://api.themoviedb.org/3/movie/11?api_key=e775d9d385ca8f76409a2109ba8ad621' -->
--- src/component/card/listingCard/listingCard.js
+++ src/component/card/listingCard/listingCard.js
@@ -1,16 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./listingCard.css";
 export default function ListingCard() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [upcomingMovieList, setUpcomingMovieList] = useState([]);
+
+  useEffect(() => {
+    getUpcomingMovieList();
+  }, []);
+
+  async function getUpcomingMovieList() {
+    setIsLoading(true);
+    try {
+      const listingKey = `${process.env.REACT_APP_LISTING_API_KEY}`;
+      console.log(listingKey);
+      const getData = await fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?page=2`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${process.env.REACT_APP_LISTING_TOKEN}`,
+          },
+        }
+      )
+        .then((res) => res.json())
+        .then((jsonRes) => {
+          console.log(jsonRes);
+          setUpcomingMovieList(jsonRes.results);
+          setIsLoading(false);
+          return jsonRes;
+        });
+    } catch (error) {
+      console.log(error);
+      alert("Internal Server Error");
+      setIsLoading(false);
+    }
+  }
+
   return (
     <div className="listing-cards-container">
-      <div className="listing-card">
-        <img className="listing-poster"></img>
-        <div className="title-rating-container">
-          <div className="listing-title">MOvie Title</div>
-          <div className="listing-ratings">Rating</div>
-        </div>
-        <div className="listing-description">description</div>
-      </div>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        upcomingMovieList.map((movie) => {
+          console.log(movie.poster_path.split("/")[1]);
+          return (
+            <div className="listing-card">
+              <img
+                className="listing-poster"
+                src={`http://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              ></img>
+              <div className="title-rating-container">
+                <div className="listing-title">{movie.title}</div>
+                <div className="listing-ratings">{movie.vote_average}</div>
+              </div>
+              <div className="listing-description">{movie.overview}</div>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 }
