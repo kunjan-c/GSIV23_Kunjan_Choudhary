6a43817ae5d36a6e3de88a2c2030fc7a284a7221
Kunjan <kunjan@controlshift.in>
2023-08-23 23:40:11 +0530

    basic project structure created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
-import './App.css';
+import ListPage from "pages/listPage/listPage";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     hi
+      <ListPage></ListPage>
     </div>
   );
 }
